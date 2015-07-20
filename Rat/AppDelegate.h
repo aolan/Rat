39849cc9e7bbc3b652eb1ff724aafd493776e18a
lawn <adcaowei@163.com>
2015-07-20 15:08:24 +0800

    Test

---
+++ Rat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Rat
+//
+//  Created by lawn on 15/7/20.
+//  Copyright (c) 2015年 zodiac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Rat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Rat
+//
+//  Created by lawn on 15/7/20.
+//  Copyright (c) 2015年 zodiac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
