d1937c73bfc90080be62250436daebdcb834b1dc
lawn <adcaowei@163.com>
2015-07-20 16:08:53 +0800

    test gitignore

--- Rat/RTAppDelegate.m
+++ Rat/RTAppDelegate.m
@@ -17,6 +17,11 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    _window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    _window.backgroundColor = [UIColor whiteColor];
+    [_window makeKeyAndVisible];
+    
     return YES;
 }
 
